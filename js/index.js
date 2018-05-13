d3fd6dac378c4aa5a80c70753d7346f2d8f61119
nataliercraun <craun@usc.edu>
2018-05-12 18:14:23 -0600

    Navbar transition working

---
+++ js/index.js
@@ -0,0 +1,43 @@
+function about() {
+
+	var t_margin = document.getElementById('link_row').style.marginTop; 
+
+	if (t_margin == '30%') {
+		document.getElementById('link_row').style.marginTop = '2%'; 
+	} else {
+		document.getElementById('link_row').style.marginTop = '30%';
+	}
+}
+
+function resume() {
+
+	var t_margin = document.getElementById('link_row').style.marginTop; 
+
+	if (t_margin == '30%') {
+		document.getElementById('link_row').style.marginTop = '2%'; 
+	} else {
+		document.getElementById('link_row').style.marginTop = '30%'; 
+	}
+}
+
+function projects() {
+
+	var t_margin = document.getElementById('link_row').style.marginTop; 
+
+	if (t_margin == '30%') {
+		document.getElementById('link_row').style.marginTop = '2%'; 
+	} else {
+		document.getElementById('link_row').style.marginTop = '30%'; 
+	}
+}
+
+function skills() {
+
+	var t_margin = document.getElementById('link_row').style.marginTop; 
+
+	if (t_margin == '30%') {
+		document.getElementById('link_row').style.marginTop = '2%'; 
+	} else {
+		document.getElementById('link_row').style.marginTop = '30%'; 
+	}
+}
