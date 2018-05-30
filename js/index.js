d52faa22889e13030e48c49dd2a83617a3e1e4fa
nataliercraun <craun@usc.edu>
2018-05-30 13:32:26 -0500

    Add button disable again

--- js/index.js
+++ js/index.js
@@ -2,6 +2,9 @@ function about() {
 
 	var about = document.getElementsByClassName("aboutCard");
 	var abtBtn = document.getElementById('abtBtn'); 
+	var resBtn = document.getElementById('resBtn');
+	var projBtn = document.getElementById('projBtn');
+	var skillBtn = document.getElementById('skillBtn');
 
 	if (abtBtn.innerHTML != 'Close') {
 		for (var i = 0; i < about.length; i++) {
@@ -11,6 +14,10 @@ function about() {
 		abtBtn.style.color = '#618685';
 		abtBtn.style.border = 'solid #618685 2px';  
 		abtBtn.style.borderRadius = '25px';
+
+		resBtn.style.pointerEvents = 'none';
+		projBtn.style.pointerEvents = 'none';
+		skillBtn.style.pointerEvents = 'none';
 	} else {
 		for (var i = 0; i < about.length; i++) {
 			about[i].style.marginTop = '-200px';
@@ -19,5 +26,9 @@ function about() {
 		abtBtn.style.color = 'black';
 		abtBtn.style.border = '';  
 		abtBtn.style.borderRadius = '';
+
+		resBtn.style.pointerEvents = '';
+		projBtn.style.pointerEvents = '';
+		skillBtn.style.pointerEvents = '';
 	}
 }
