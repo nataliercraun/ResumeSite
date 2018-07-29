b99ea475a40749a210f15337fd539c172a7f7309
nataliercraun <craun@usc.edu>
2018-07-29 16:49:58 -0500

    Change summary js entry pattern

--- js/index.js
+++ js/index.js
@@ -1,87 +1,53 @@
+
+// Pull in personal summary from off of the left-side of the page 
 function about() {
 
-	var about = document.getElementsByClassName("aboutCard");
-	var bio = document.getElementById("summary"); 
+	var summary = document.getElementById('summary'); 
+	var social = document.getElementById('social-media'); 
+
+	// Get all navbar buttons 
 	var abtBtn = document.getElementById('abtBtn'); 
 	var resBtn = document.getElementById('resBtn');
 	var projBtn = document.getElementById('projBtn');
 
 	if (abtBtn.innerHTML != 'Close') {
-		for (var i = 0; i < about.length; i++) {
-			about[i].style.marginTop = '10px';
-		}
+		console.log('here'); 
+		social.style.marginTop = '300px'; 
+		summary.style.left = '200px'; 
 
-		// Bio appears
-		bio.style.display = 'flex';
-
+		// Button stuff 
 		abtBtn.innerHTML = 'Close';
 		abtBtn.style.color = '#618685';
 		abtBtn.style.border = 'solid #618685 2px';  
 		abtBtn.style.borderRadius = '25px';
-
-		resBtn.style.pointerEvents = 'none';
 		projBtn.style.pointerEvents = 'none';
+		resBtn.style.pointerEvents = 'none';
 	} else {
-		for (var i = 0; i < about.length; i++) {
-			about[i].style.marginTop = '-200px';
-		}
 
-		// Bio disappears
-		bio.style.display = 'none';
-
+		social.style.marginTop = '25px'; 
+		summary.style.left = '-10000px'; 
+		// Button stuff 
 		abtBtn.innerHTML = 'About';
 		abtBtn.style.color = 'black';
 		abtBtn.style.border = '';  
 		abtBtn.style.borderRadius = '';
 
-		resBtn.style.pointerEvents = '';
 		projBtn.style.pointerEvents = '';
+		resBtn.style.pointerEvents = '';
 	}
+	
 }
 
 function resume() {
-	var about = document.getElementsByClassName("aboutCard");
-	var abtBtn = document.getElementById('abtBtn'); 
-	var resBtn = document.getElementById('resBtn');
-	var projBtn = document.getElementById('projBtn');
-
-	if (resBtn.innerHTML != 'Close') {
-		for (var i = 0; i < about.length; i++) {
-			about[i].style.marginTop = '10px';
-		}
-
-		// Resume comes up from bottom 
-		var res = document.getElementById('resume');
-		res.style.display = 'flex'; 
-
-		resBtn.innerHTML = 'Close';
-		resBtn.style.color = '#618685';
-		resBtn.style.border = 'solid #618685 2px';  
-		resBtn.style.borderRadius = '25px';
-
-		abtBtn.style.pointerEvents = 'none';
-		projBtn.style.pointerEvents = 'none';
-	} else {
-		for (var i = 0; i < about.length; i++) {
-			about[i].style.marginTop = '-200px';
-		}
-
-		//Resume moves to bottom and is hidden
-		var res = document.getElementById('resume');
-		res.style.display = 'none'; 
-		
-		resBtn.innerHTML = 'Resume';
-		resBtn.style.color = 'black';
-		resBtn.style.border = '';  
-		resBtn.style.borderRadius = '';
-
-		abtBtn.style.pointerEvents = '';
-		projBtn.style.pointerEvents = '';
-	}
+	
 }
 
+
+// Pull down projects info and move nav bar to accomodate 
 function projects() {
 	var about = document.getElementsByClassName("aboutCard");
+
+	// Get all navbar buttons 
 	var abtBtn = document.getElementById('abtBtn'); 
 	var resBtn = document.getElementById('resBtn');
 	var projBtn = document.getElementById('projBtn');
@@ -91,11 +57,12 @@ function projects() {
 			about[i].style.marginTop = '10px';
 			about[i].style.visibility = 'visible'; 
 		}
+
+		// Update navbar buttons 
 		projBtn.innerHTML = 'Close';
 		projBtn.style.color = '#618685';
 		projBtn.style.border = 'solid #618685 2px';  
 		projBtn.style.borderRadius = '25px';
-
 		abtBtn.style.pointerEvents = 'none';
 		resBtn.style.pointerEvents = 'none';
 	} else {
@@ -103,12 +70,22 @@ function projects() {
 			about[i].style.marginTop = '-200px';
 			about[i].style.visibility = 'hidden'; 
 		}
+
+		// Update navbar buttons 
 		projBtn.innerHTML = 'Projects';
 		projBtn.style.color = 'black';
 		projBtn.style.border = '';  
 		projBtn.style.borderRadius = '';
-
 		abtBtn.style.pointerEvents = '';
 		resBtn.style.pointerEvents = '';
 	}
 }
+
+
+
+
+
+
+
+
+
